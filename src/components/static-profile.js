63873229e77bbd0d0ae1416ba062b8e993faf7c4
Eshwari Bhide <ebhide99@gmail.com>
2021-04-02 15:38:33 -0700

    removing sensitive information and editing fields in static profile

--- src/components/static-profile.js
+++ src/components/static-profile.js
@@ -4,47 +4,14 @@ const StaticProfile = () => {
     return(
         <div class="container">
             <h1>Profile</h1>
-                <form>
-                    <div class="form-group row">
-                        <label for="username" class="col-sm-2 col-form-label">
-                            Username </label>
-                        <div class="col-sm-6">
-                            <input class="form-control wbdv-field wbdv-username"
-                                   id="username"
-                                   readonly
-                                   value="Alice"/>
+            <br/>
+                    <div class="row">
+                        <div class="col-4">
+                            <h2> "Insert username here" </h2>
                         </div>
                     </div>
-                    <div class="form-group row">
-                        <label for="phone" class="col-sm-2 col-form-label">
-                            Phone </label>
-                        <div class="col-sm-6">
-                            <input class="form-control wbdv-field wbdv-phone"
-                                   type="tel"
-                                   id="phone"
-                                   value="(555) 123-4324"/>
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="email" class="col-sm-2 col-form-label">
-                            Email </label>
-                        <div class="col-sm-6">
-                            <input class="form-control wbdv-field email"
-                                   type="email"
-                                   id="email"
-                                   value="alice@wonderland.com"/>
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label"></label>
-                        <div class="col-sm-6">
-                            <a class="btn btn-success btn-block"
-                               href="#">
-                                Update
-                            </a>
-                        </div>
-                    </div>
-                </form>
+                <br/>
+                <br/>
                 <br/>
                 <div class="row">
                         <div class="col-4">
@@ -54,9 +21,9 @@ const StaticProfile = () => {
                             <h2>Followers</h2>
                         </div>
                         <div class="col-4">
-                            <h2>Your Last Reviews</h2>
+                            <h2>Last Reviews</h2>
                         </div>
-                    </div>
+                </div>
         </div>
     )
 }
