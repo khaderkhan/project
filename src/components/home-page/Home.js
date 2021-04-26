2f08a21f5e649dd017d5baafda5be970af103e6d
Eshwari Bhide <ebhide99@gmail.com>
2021-04-26 13:09:28 -0700

    dynamic content for home page even logged out

--- src/components/home-page/Home.js
+++ src/components/home-page/Home.js
@@ -10,12 +10,18 @@ import {read_cookie} from "sfcookies";
 const Home = () => {
 
     const [recentReviews, setRecentReviews] = useState([])
+    const [reviewsByUser, setReviewsByUser] = useState(({reviewsByUser: []}))
     const [isLoggedIn, setLoggedIn] = useState(false);
 
 
     useEffect(() => {
+        const userID = read_cookie("userID")
         reviewService.findAllRecentReviews()
             .then(reviews => setRecentReviews(reviews));
+        reviewService.findAllReviewsForUser(userID).then(
+        response => {
+            setReviewsByUser(response)
+        })
         if (read_cookie("loginCookie") === true) {
             setLoggedIn(true)
         }
@@ -25,13 +31,33 @@ const Home = () => {
         <div>
             <MovieSlide/>
             {
-                isLoggedIn &&
+                !isLoggedIn &&
 
                 <div className="row">
                     <div className="col-6 mt-5 ml-2 mb-1 mr-3">
                         <h3>Most Recent Reviews by Reviewers</h3>
                         {
                             recentReviews.map((rev) => {
+                                return (
+                                    <div key={rev._id} className="mb-1">
+                                        <ReviewCard review={rev}/>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+
+                </div>
+            }
+
+            {
+                isLoggedIn &&
+
+                <div className="row">
+                    <div className="col-6 mt-5 ml-2 mb-1 mr-3">
+                        <h3>Your Most Recent Reviews</h3>
+                        {
+                            reviewsByUser.reviews && reviewsByUser.reviews.map((rev) => {
                                 return (
                                     <div key={rev._id} className="mb-1">
                                         <ReviewCard review={rev}/>
