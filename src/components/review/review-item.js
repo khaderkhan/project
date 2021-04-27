0d4cc8c7b42b0f0056732d179156f6111b02dc54
Mubaris Khan <mubaris.khan786@gmail.com>
2021-04-27 17:49:38 -0400

    comment fix

--- src/components/review/review-item.js
+++ src/components/review/review-item.js
@@ -39,15 +39,15 @@ const ReviewItem = (
     const [userFname, setUserFname] = useState('')
     const [userLname, setUserLname] = useState('')
     const [comment, setComment] = useState('')
-    
-    
+    const [commentToggle, setCommentToggle] = useState(true)
+    const [tempComment, setTempComment] = useState('')
 
     useEffect(() => {
 
                  rev.userID && userService.findUserById(rev.userID)
                      .then(user => {setUserFname(user.firstName); setUserLname(user.lastName)})
 
-             }, [])
+             }, [commentToggle, tempComment])
     
     const onFinish = (values) => {
         const commentObj = {
@@ -56,10 +56,15 @@ const ReviewItem = (
             userId: rev.userID,
             movieId: rev.movieId
         }
+        console.log("inside the onfinish thing", values)
         commentService.createComment(commentObj).then( res => {
             setComment(values.comment)
         })
-       
+        console.log("here==>> tempComment", tempComment.length, values.comment)
+        setTempComment(values.comment)
+        console.log("tempComment.length=====>>", tempComment)
+        setCommentToggle(false)
+        
         
         // Invoke the comment service code that will send the comment to the backend. 
         // Save the comment in a state variable. 
@@ -89,7 +94,7 @@ const ReviewItem = (
                         <Divider />
                         {
                         
-                        rev.comment.length === 0 &&
+                        rev.comment.length === 0 && commentToggle &&
                         <>
                         
                             <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
@@ -109,7 +114,16 @@ const ReviewItem = (
                             <>
                                 <h4>Comment</h4>
                                 <p>
-                                 {rev.comment[0].comment}
+                                 {tempComment.length == 0? rev.comment[0].comment: tempComment}
+                                </p>
+                            </>
+                        }
+                        {
+                            tempComment.length !== 0 &&
+                            <>
+                                <h4>Comment</h4>
+                                <p>
+                                 {tempComment}
                                 </p>
                             </>
                         }
